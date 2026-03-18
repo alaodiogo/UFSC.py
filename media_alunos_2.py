d53738c8b81e0aad64ec1b98363054ed4f49e536
Diogo Oliveira <89609323+alaodiogo@users.noreply.github.com>
2026-03-18 19:33:22 -0300

    Add files via upload

---
+++ media_alunos_2.py
@@ -0,0 +1,12 @@
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+
+peso_1 = 2
+peso_2 = 3
+peso_3 = 5
+soma_pesos = peso_1+peso_2+peso_3
+
+media = ((n1*peso_1) + (n2*peso_2) + (n3*peso_3)) / soma_pesos # Calculo da media do aluno com o peso
+
+print (f"MEDIA = {media}")
