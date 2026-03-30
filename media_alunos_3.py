2bf962b6c8f97031d08c8c4bed45fed7d795025d
Diogo Oliveira <89609323+alaodiogo@users.noreply.github.com>
2026-03-30 01:24:43 -0300

    Add files via upload

---
+++ media_alunos_3.py
@@ -0,0 +1,12 @@
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+peso_1 = float(input())
+peso_2 = float(input())
+peso_3 = float(input())
+
+soma_pesos = peso_1 + peso_2 + peso_3
+
+media = ((n1*peso_1) + (n2*peso_2) + (n3*peso_3)) / soma_pesos
+
+print (media >= 6.0)
