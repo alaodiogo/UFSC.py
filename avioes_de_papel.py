516867dfd5e35fde0dc63b7f088f758ac2c9aa01
Diogo Oliveira <89609323+alaodiogo@users.noreply.github.com>
2026-04-13 21:27:12 -0300

    Update avioes_de_papel.py

--- avioes_de_papel.py
+++ avioes_de_papel.py
@@ -1,8 +1,8 @@
 num_competidores, qtd_total_folhas, qtd_folhas_competidor = [int(w) for w in input().split()]
 
-papel_suficiente = qtd_total_folhas // num_competidores // qtd_folhas_competidor
+o_papel_e_suficiente = qtd_total_folhas // num_competidores // qtd_folhas_competidor
 
-if papel_suficiente > 0:
+if o_papel_e_suficiente > 0:
     print ("S")
 else:
     print ("N")
